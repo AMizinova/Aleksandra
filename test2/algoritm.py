898660b949cad8b7c3446073ed50260f7ec172d4
Aleksandra <aleksandra_miz@email.com>
2021-05-21 23:31:03 +0300

    изменение3

--- test2/algoritm.py
+++ test2/algoritm.py
@@ -2,16 +2,40 @@
 Разработка умного помощника для выбора автомобилей ГАЗа в телеграмме
    Поключение чат бота к БД сайта ГАЗа, чтобы брать оттуда информацию по модельному ряду автомобилей (можно использовать json)
 """
-import pdfminer.six
+import pdfminer
 #извлечение текста из пдф
-import PyPDF2
-with open('bd.pdf', 'rb') as pdf_file: pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-# printing first page contents 
-pdf_page = pdf_reader.getPage(0) 
-print(pdf_page.extractText()) 
-# reading all the pages content one by one 
-for page_num in range(pdf_reader.numPages): pdf_page = pdf_reader.getPage(page_num) 
-print(pdf_page.extractText())
+import io
+ 
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+ 
+def extract_text_by_page(pdf_path):
+    with open(pdf_path, 'rb') as fh:
+        for page in PDFPage.get_pages(fh, 
+                                      caching=True,
+                                      check_extractable=True):
+            resource_manager = PDFResourceManager()
+            fake_file_handle = io.StringIO()
+            converter = TextConverter(resource_manager, fake_file_handle)
+            page_interpreter = PDFPageInterpreter(resource_manager, converter)
+            page_interpreter.process_page(page)
+ 
+            text = fake_file_handle.getvalue()
+            yield text
+ 
+            # close open handles
+            converter.close()
+            fake_file_handle.close()
+ 
+def extract_text(pdf_path):
+    for page in extract_text_by_page(pdf_path):
+        print(page)
+        print()
+ 
+if __name__ == '__main__':
+    print(extract_text('bd.pdf'))
 
 import telebot
 
